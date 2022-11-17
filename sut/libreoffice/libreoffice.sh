e870719e2f9d180af3ead05c77d5c7862fc6e7d8
Gerardo Vitagliano <vitaglianog@gmail.com>
2022-11-17 12:01:09 +0000

    libreoffice script

--- sut/libreoffice/libreoffice.sh
+++ sut/libreoffice/libreoffice.sh
@@ -28,48 +28,47 @@ function to_xlsx(){
 cd "$IN_DIR"
 
 
-#for f in file_field_delimiter*
-#do
-#    tmp="${f%.csv*}"
-#    tmp="${tmp#*file_field_delimiter_}"
-#    IFS='_' read -r -a array <<< "$tmp"
+for f in file_field_delimiter*
+do
+    tmp="${f%.csv*}"
+    tmp="${tmp#*file_field_delimiter_}"
+    IFS='_' read -r -a array <<< "$tmp"
 
-#    delimiters=$(printf "/%d" "${array[@]}")
-#    delimiters=${delimiters:1}
-#    MRG="/MRG"
+    delimiters=$(printf "/%d" "${array[@]}")
+    delimiters=${delimiters:1}
+    MRG="/MRG"
 
-#    [[ "${#array[@]}" -gt "1" ]] && parameters={"$delimiters$MRG,34,ASCII"} || parameters={"$delimiters,34,ASCII"}
+    [[ "${#array[@]}" -gt "1" ]] && parameters={"$delimiters$MRG,34,ASCII"} || parameters={"$delimiters,34,ASCII"}
 
-#    to_xlsx "$f" "$parameters"
-#done
+    to_xlsx "$f" "$parameters"
+done
 
-#for f in file_quotation_char*
-#do
-#    tmp="${f%.csv*}"
-#    tmp="${tmp#*table_quotation_char_}"
-#    IFS='_' read -r -a array <<< "$tmp"
-#    quotations=$(printf "%d" "${array[0]}") #Libreoffice only has one quotation mark, so we take the first
-#    parameters={"44,$quotations,ASCII"}
-#    to_xlsx "$f" "$parameters"
-#done
+for f in file_quotation_char*
+do
+    tmp="${f%.csv*}"
+    tmp="${tmp#*table_quotation_char_}"
+    IFS='_' read -r -a array <<< "$tmp"
+    quotations=$(printf "%d" "${array[0]}") #Libreoffice only has one quotation mark, so we take the first
+    parameters={"44,$quotations,ASCII"}
+    to_xlsx "$f" "$parameters"
+done
 
-#for f in file_double_trailing* file_header* file_multitable* file_no* file_one* file_preamble* file_record* row_extra* row_less* row_more* file_escape_char*
-for f in file_escape_char*
+for f in file_double_trailing* file_header* file_multitable* file_no* file_one* file_preamble* file_record* row_extra* row_less* row_more* file_escape_char*
 do
     to_xlsx "$f"
 done
 
 to_xlsx source.csv
-#for f in row_field_delimiter_*
-#do
-#    tmp="${f%.csv*}"
-#    tmp="${tmp#*0x}"
-#    IFS='_' read -r -a array <<< "0x$tmp"
-#    delimiters=$(printf "%d/" "${array[@]}")
-#    delimiters=${delimiters::-1}
-#    parameters={"$delimiters,34,ASCII"}
-#    to_xlsx "$f" "$parameters"
-#done
+for f in row_field_delimiter_*
+do
+    tmp="${f%.csv*}"
+    tmp="${tmp#*0x}"
+    IFS='_' read -r -a array <<< "0x$tmp"
+    delimiters=$(printf "%d/" "${array[@]}")
+    delimiters=${delimiters::-1}
+    parameters={"$delimiters,34,ASCII"}
+    to_xlsx "$f" "$parameters"
+done
 
 for file in "$TMP_DIR"*.xlsx
 do
