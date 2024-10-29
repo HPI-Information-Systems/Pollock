402f31e0e12ba9e97717ed20c4a49c959de6d02c
pdet <pedroholanda@gmail.com>
2024-10-29 13:12:46 +0100

    wip"
     "

--- sut/duckdb/duck-bench.py
+++ sut/duckdb/duck-bench.py
@@ -3,40 +3,10 @@ import duckdb
 import time
 import os
 from utils import print, save_time_df, load_parameters
-"""
-(filepath_or_buffer, 
-delimiter=None, Delimiter to use. If sep is None, the Python parsing engine will be used and automatically detect the separator.
-            In addition, separators longer than 1 character and different from '\s+' will be interpreted as regex. 
-            Note that regex delimiters are prone to ignoring quoted data. Regex example: '\r\t'.
-header='infer', Row number(s) to use as the column names, and the start of the data. 
-prefix=None, 
-mangle_dupe_cols=True, 
-skipinitialspace=False, 
-skiprows=None, Line numbers to skip (0-indexed) or number of lines to skip (int) at the start of the file.
-skipfooter=0, Number of lines at bottom of file to skip 
-nrows=None, 
-na_values=None, 
-keep_default_na=True, 
-na_filter=True,
-skip_blank_lines=True, 
-parse_dates=False, 
-infer_datetime_format=False, 
-keep_date_col=False, 
-date_parser=None, 
-dayfirst=False,
-thousands=None, 
-decimal='.', 
-quotechar='"',
-doublequote=True, 
-escapechar=None, 
-comment=None, 
-encoding=None, 
-error_bad_lines=True, 
-warn_bad_lines=True,
-low_memory=True"""
+import pandas as pd
 
 sut='duckdb'
-DATASET = os.environ['DATASET']
+DATASET = 'polluted_files'
 IN_DIR = abspath(f'/{DATASET}/csv/')
 PARAM_DIR = abspath(f'/{DATASET}/parameters')
 OUT_DIR = abspath(f'/results/{sut}/{DATASET}/loading/')
@@ -59,9 +29,10 @@ for idx,file in enumerate(benchmark_files):
         con = duckdb.connect()
         start = time.time()
         try:
-            rel = con.from_query(f"SELECT * from read_csv_auto('{in_filepath}', ignore_errors=true)")
+            rel = con.from_query(f"FROM read_csv('{in_filepath}', ignore_errors=true)")
             end = time.time()
-            rel.write_csv(out_filepath, index=False)
+            rel = rel.df()
+            rel.to_csv(out_filepath, index=False)
         except Exception as e:
             end = time.time()
             print("\t", e)
